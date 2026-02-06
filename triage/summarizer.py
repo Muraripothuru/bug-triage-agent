1b0282dfe86ef23ccbe793d92149b7ca0a7ed969
Muraripothuru <murari.pothuru098@gmail.com>
2026-02-06 15:31:09 +0530

    Implement issue summarization function
    Add a function to summarize bug issues with title, description, and comments.

--- triage/summarizer.py
+++ triage/summarizer.py
@@ -1,1 +1,15 @@
+def summarize_issue(bug):
+    title = bug.get("title", "")
+    description = bug.get("description", "")
+    comments = bug.get("comments", [])
+
+    summary = title
+
+    if description:
+        summary += " - " + description[:80]
+
+    if comments:
+        summary += " | Key note: " + comments[0]
+
+    return summary
 
